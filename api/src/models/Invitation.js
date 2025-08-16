242578443c57fa4db05b3a9ee4f8261aeee02ef9
Dimas Rizky <dimasrizkym3@gmail.com>
2025-08-16 08:43:03 +0700

    feat: Invitation Schema model

---
+++ api/src/models/Invitation.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const invitationSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Projects",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Invitation = mongoose.model("Invitation", invitationSchema);
+
+export default Invitation;
